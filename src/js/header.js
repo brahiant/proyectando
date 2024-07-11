3d3ded7daa2b326fc64fba70b0dedd09311526ac
BrahianT <brahiant0@gmail.com>
2024-07-11 14:04:39 -0500

    Se organiza los archivos js

--- src/js/header.js
+++ src/js/header.js
@@ -6,3 +6,27 @@ document.addEventListener('DOMContentLoaded', () => {
         navbarMenu.classList.toggle('active');
     });
 });
+
+async function getListNavbarMenu() {
+    const listMenu = [
+        { name: 'Inicio', icon: 'bi bi-house' },
+        { name: 'Quiénes somos', icon: 'bi bi-people' },
+        { name: 'Servicios', icon: 'bi bi-briefcase' },
+        { name: 'Contacto', icon: 'bi bi-envelope' }
+    ];
+
+    const ul = document.querySelector('#listNavbar');
+    if (!ul) {
+        console.error('Elemento de lista del navbar no encontrado');
+        return;
+    }
+
+    ul.innerHTML = '';
+    listMenu.forEach(element => {
+        let newLi = document.createElement('li');
+        newLi.innerHTML = `<button type="button" class="btn btn-outline-primary"><i class="${element.icon}"></i>${element.name}</button>`;
+        ul.appendChild(newLi);
+    });
+}
+
+getListNavbarMenu();
--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,3 @@
-async function getListNavbarMenu() {
-    const listMenu = [
-        { name: 'Inicio', icon: 'bi bi-house' },
-        { name: 'Quiénes somos', icon: 'bi bi-people' },
-        { name: 'Servicios', icon: 'bi bi-briefcase' },
-        { name: 'Contacto', icon: 'bi bi-envelope' }
-    ];
-
-    const ul = document.querySelector('#listNavbar');
-    if (!ul) {
-        console.error('Elemento de lista del navbar no encontrado');
-        return;
-    }
-
-    ul.innerHTML = '';
-    listMenu.forEach(element => {
-        let newLi = document.createElement('li');
-        newLi.innerHTML = `<button type="button" class="btn btn-outline-primary"><i class="${element.icon}"></i>${element.name}</button>`;
-        ul.appendChild(newLi);
-    });
-}
-
 async function getListProyect() {
     const count = 4;
     const div = document.querySelector('#proyects');
@@ -37,6 +15,4 @@ async function getListProyect() {
     }
 }
 
-
-getListNavbarMenu();
 getListProyect();
